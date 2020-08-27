ea5fb8192d50b3d4e7bd5b5a4fe5bff062bc5927
Cheolho Jang <CJang@wildcatdiscovery.com>
2020-08-26 18:47:55 -0700

    init

---
+++ kitchenstory/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace kitchenstory.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name = "User")
+        {
+            string current_date = DateTime.Today.ToShortDateString();
+            ViewData["Message"] = "Welcome, " + name;
+            ViewData["Date_message"] = "Today's date is ..." + current_date;
+            return View();
+        }
+    }
+}
